c83fa01a988792cf914e96414814ace51cddbebe
Erin Cook <erincook@ymaill.com>
2016-06-23 10:48:15 -0400

    almanac placeholder.

--- docroot/templates/pages/today/today.js
+++ docroot/templates/pages/today/today.js
@@ -1,4 +1,4 @@
 (function(){
     helper.loadTemplate('today-forecast', 'modules', 'today-forecast');
-    helper.loadTemplate('static-map-div', 'modules', 'static-map');
+    helper.loadTemplate('today-almanac', 'modules', 'almanac');
 })();
