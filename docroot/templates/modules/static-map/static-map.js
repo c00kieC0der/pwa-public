bd89b441118b4bf99087dfb261d5118ce85da361
Erin Cook <erincook@ymaill.com>
2016-06-22 22:03:39 -0400

    save progress on map toggles.

--- docroot/templates/modules/static-map/static-map.js
+++ docroot/templates/modules/static-map/static-map.js
@@ -1,18 +1,22 @@
-var map = window._map = new wx.maps.Map(document.getElementsByClassName('static-map')[0], new wx.maps.MapOptions({
-    zoomLevel: 7,
-    latitude: _User.activeLocation.lat,
-    longitude: _User.activeLocation.long
-}));
 
-window.addLayer = function (index) {
-    var layer = map.createLayer(new wx.layers.SunTileLayerOptions(layers[index]));
-    map.stack[1] && map.removeLayer(map.stack[1]);
-    map.addLayer(layer);
-    $("input").each(function (i, el) {
-        var $el = $(el);
-        $el.css('display', ($el.val() == layer.options.layerKey) ? 'none' : 'block');
-    });
-};
+(function() {
+    document.getElementById('event-anchor').addEventListener('builder', function () {
+
+        var map = window._map = new wx.maps.Map(document.getElementsByClassName('static-map')[0], new wx.maps.MapOptions({
+            zoomLevel: 7,
+            latitude: _User.activeLocation.lat,
+            longitude: _User.activeLocation.long
+        }));
+
+        window.addLayer = function (index) {
+            var layer = map.createLayer(new wx.layers.SunTileLayerOptions(layers[index]));
+            map.stack[1] && map.removeLayer(map.stack[1]);
+            map.addLayer(layer);
+            $("input").each(function (i, el) {
+                var $el = $(el);
+                $el.css('display', ($el.val() == layer.options.layerKey) ? 'none' : 'block');
+            });
+        };
 
         var layers = [{
             id: "SUN Radar Observation",
@@ -34,23 +38,66 @@ window.addLayer = function (index) {
             version: "2"
         }];
 
-    document.getElementById('event-anchor').addEventListener('builder', function() {
+        document.getElementById('event-anchor').addEventListener('builder', function () {
 
 
+            map.configuration.mapboxAccessToken = "pk.eyJ1Ijoid2VhdGhlciIsImEiOiJjaWlxNG01czkwMjM2dnFtNTdlMjVidTByIn0.Ml63Jx_BQtTx4CEXihwjyA";
+            map.configuration.sunTileProductsUrl = "//api.weather.com/v2/TileServer/series?apiKey={apiKey}";
+            map.configuration.sunApiKey = "3d498bd0777076fb2aa967aa67114c7e";
 
+            // Base Layer
+            var baseLayer = map.createLayer(new wx.layers.MapboxTileLayerOptions({
+                id: "Mapbox Streets",
+                mapBoxId: "mapbox.streets",
+                minimumZoom: 0,
+                maximumZoom: 19
+            }));
+            map.addLayer(baseLayer);
+            addLayer(0);
+        });
 
-
-        map.configuration.mapboxAccessToken = "pk.eyJ1Ijoid2VhdGhlciIsImEiOiJjaWlxNG01czkwMjM2dnFtNTdlMjVidTByIn0.Ml63Jx_BQtTx4CEXihwjyA";
-        map.configuration.sunTileProductsUrl = "//api.weather.com/v2/TileServer/series?apiKey={apiKey}";
-        map.configuration.sunApiKey = "3d498bd0777076fb2aa967aa67114c7e";
-
-// Base Layer
-        var baseLayer = map.createLayer(new wx.layers.MapboxTileLayerOptions({
-            id: "Mapbox Streets",
-            mapBoxId: "mapbox.streets",
-            minimumZoom: 0,
-            maximumZoom: 19
+        // Insert a marker
+        var overlayLayer = map.createLayer(new wx.layers.OverlayLayerOptions({
+            id: "imageMarker"
         }));
-        map.addLayer(baseLayer);
-        addLayer(0);
+        var image = "https://s.w-x.co/map-pin-hex-393939-v1.png";
+        var point = map.geoCenter;
+        var options = new wx.overlays.ImageMarkerOptions({
+            size: new wx.Size(26, 33),
+            title: "Image Marker",
+            isClickable: false
+        });
+        var overlay = new wx.overlays.ImageMarker(point, image, options);
+        map.addLayer(overlayLayer);
+        overlayLayer.addOverlay(overlay);
+
+
+     //   map.doubleClickZoom.disable();
+     //   map.scrollWheelZoom.disable();
+     //   map.dragging.disable();
     });
+})();
+var dropdownShowing = false;
+var menuShowHide = function(){
+    if(dropdownShowing){
+        showHide('menuDropdown', 0);
+        dropdownShowing = false;
+        addRemoveActiveClass('menuTitle', 0);
+    } else {
+        showHide('menuDropdown', 1);
+        dropdownShowing = true;
+        addRemoveActiveClass('menuTitle', 1);
+    }
+};
+var addRemoveActiveClass = function(ele, add){
+    if(add){
+        document.getElementById(ele).className += ' active';
+    } else {
+        document.getElementById(ele).className = document.getElementById(ele).className.replace('active', '');
+    }
+}
+var addAMapLayer = function(layerKey){
+    menuShowHide();
+    addLayer(layerKey);
+};
+
--- docroot/templates/pages/today/today.js
+++ docroot/templates/pages/today/today.js
@@ -1,4 +1,3 @@
 (function(){
     helper.loadTemplate('today-forecast', 'modules', 'today-forecast');
-    helper.loadTemplate('themapyo', 'modules', 'static-map');
 })();
