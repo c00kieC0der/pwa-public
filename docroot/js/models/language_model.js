c8ba393d5ebf8b1ec110920d7d5b5bff5d70d5ac
Avery.horton <avery.horton@cgardner-ml.local>
2016-06-23 08:37:40 -0400

    Added strings for translation in language_model.js

--- docroot/js/models/language_model.js
+++ docroot/js/models/language_model.js
@@ -9,7 +9,116 @@ var pathMap = {
 };
 var privateFilePath = pathMap[document.domain];
 
+//TODO: Remove comments
+var translationStrings = {
+    //Nav Headers
+    'TODAY':'TODAY',
+    'HOURLY':'HOURLY',
+    '10 DAY':'10 DAY',
+    '5 DAY':'5 DAY',
+    'MONTHLY':'MONTHLY',
+    'Almanac':'Almanac',
+    //Footer Content:
+    'Terms of Use':'Terms of Use',
+    'Privacy Policy':'Privacy Policy',
+    'Parental Controls':'Parental Controls',
+    'Ad Choices':'Ad Choices',
+    //Today Page
+    'feels like':'feels like',
+    'Wind':'Wind',
+    'Humidity':'Humidity',
+    'Dew Point':'Dew Point',
+    'Pressure':'Pressure',
+    'Visibility':'Visibility',
+    'UV Index':'UV Index',
+    //Units
+    'mph':'mph',
+    'in':'in',
+    'mi':'mi',
+    'pm':'pm',
+    'am':'am',
+    //Include high and low,?
 
-//Ajax in the files we need.
+    //Hourly
+    'TIME':'TIME',
+    'DESCRIPTION':'DESCRIPTION',
+    'TEMP':'TEMP',
+    'FEELS':'FEELS',
+    'PRECIP':'PRECIP',
+    //Ask, fragment
+    'Hourly Forecast for':'Hourly Forecast for',
+    'More Hours': 'More Hours',
+    'Weather':'Weather',
+    //Navigation
+        //Dropdowns
+        //Search Icon
+    'Update Current Location':'Update Current Location',
+    'RECENT LOCATIONS':'RECENT LOCATIONS',
+    'Search city, zip, or place': 'Search city, zip, or place',
+    'Manage All Locations':'Manage All Locations',
+        //Hamburger
+        //Also has weather in all caps. Add again?
+    'MAPS':'MAPS',
+    'SEVERE':'SEVERE',
+    'ACTIVITIES':'ACTIVITIES',
+    'NEWS & PHOTOS':'NEWS & PHOTOS',
+    'HEALTH':'HEALTH',
+    'TRAVEL':'TRAVEL',
+    'LOG IN':'LOG IN',
+    'SIGN UP':'SIGN UP',
+    'CHANGE LOCATION':'CHANGE LOCATION',
+    //5/10 Day
+    '5 MORE DAYS':'5 MORE DAYS',
+    //Formats
+    "MONTH": [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ],
+    "SHORTDAY": [
+        "SUN",
+        "MON",
+        "TUE",
+        "WED",
+        //Change to 'THUR'?
+        "THUR",
+        "FRI",
+        "SAT"
+    ],
+    "SHORTMONTH": [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ],
 
 
+
+
+
+
+
+
+
+
+
+
+
+};
