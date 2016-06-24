7a2095fe58c624e893ec7931446144e2e9ab8260
Omkar D <omkar.dokur.contractor@weather.com>
2016-06-24 12:28:42 -0400

    Page toast notifications

--- docroot/js/init.js
+++ docroot/js/init.js
@@ -1,5 +1,6 @@
 //This would be the today page, on initial load. Below is just for testing.
 //helper.loadTemplate('page-content', 'pages', 'today');
+helper.loadTemplate('page-toast-div', 'modules', 'page-toast');
 
 
 ////initialize the service worker file.
