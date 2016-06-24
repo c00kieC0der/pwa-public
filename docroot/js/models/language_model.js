40486041047c03b47243fd261f26a4f3e2cab8b9
Avery.horton <avery.horton@cgardner-ml.local>
2016-06-24 09:03:02 -0400

    Added language strings for translation in language_model.js. Comments still need to be removed.

--- docroot/js/models/language_model.js
+++ docroot/js/models/language_model.js
@@ -1,6 +1,115 @@
-var translationsStrings = {
-    'Humidity' : 'Humidity',
-    'Wind' : 'Wind'
+//TODO: Remove comments
+var translationStrings = {
+    //Nav Headers
+    'today':'today',
+    'hourly':'hourly',
+    '10 day':'10 day',
+    '5 day':'5 day',
+    'monthly':'monthly',
+    'almanac':'almanac',
+    //Footer Content:
+    'terms of use':'terms of use',
+    'privacy policy':'privacy policy',
+    'parental controls':'parental controls',
+    'ad choices':'ad choices',
+    //Today Page
+    'feels like':'feels like',
+    'wind':'wind',
+    'humidity':'humidity',
+    'dew point':'dew point',
+    'pressure':'pressure',
+    'visibility':'visibility',
+    'uv index':'uv index',
+    //Units
+
+    //Hourly
+    'time':'time',
+    'description':'description',
+    'temp':'temp',
+    'feels':'feels',
+    'precip':'precip',
+    'hourly forecast for':'hourly forecast for',
+    'more hours': 'more hours',
+    'weather':'weather',
+    //Navigation
+    //Dropdowns
+    //Search Icon
+    'update current location':'update current location',
+    'recent searches':'recent searches',
+    'search city, zip, or place': 'search city, zip, or place',
+      //not on int, remove?
+    'manage all locations':'manage all locations',
+    //Hamburger
+    'maps':'maps',
+        //not on int, remove?
+    'severe':'severe',
+    //5/10 Day
+    '5 more days':'5 more days',
+    //Almanac
+    'yesterday':'yesterday',
+    'last 7 days':'last 7 days',
+    'averages':'averages',
+    'records':'records',
+    'month to date':'month to date',
+    'historical monthly avg':'historical monthly avg',
+    'max':'max',
+    'min':'min',
+    'moonrise':'moonrise',
+    'moonset':'moonset',
+    'sunrise':'sunrise',
+    'sunset':'sunset',
+    'solar noon':'solar noon',
+    'moon phase':'moon phase',
+
+    //Formats
+    "MONTH": [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ],
+    "SHORTDAY": [
+        "sun",
+        "mon",
+        "tue",
+        "wed",
+        "thur",
+        "fri",
+        "sat"
+    ],
+    "SHORTMONTH": [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ],
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
-
-
