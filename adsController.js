d98b14e8818451ca5ad121db091d4993047f0e02
Ankit Parekh <ankit.parekh@weather.com>
2016-06-27 21:49:15 -0400

    moving the ad code to the adscontroller file

--- adsController.js
+++ adsController.js
@@ -16,6 +16,195 @@ function jsonp(url, callback) {
     document.body.appendChild(script);
 }
 
+window.googletag = window.googletag || {};
+googletag.cmd = googletag.cmd || [];
+window.ls_dfp_slots = window.ls_dfp_slots || [];
+
+window.renderStartTime = new Date().getTime();
+console.dir(window.renderStartTime);
+window.TWC = window.TWC || {};
+TWC.adUtils = TWC.adUtils || {};
+
+TWC.adUtils.addLoadEvent = function(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+};
+
+TWC.Deferred = function() {
+    var _self = this;
+    this.resolve = null;
+    this.reject = null;
+    this.promise = new Promise(function(resolve, reject) {
+        _self.resolve = resolve;
+        _self.reject = reject;
+    });
+};
+
+!function($$) {
+
+    var isMobile = window.innerWidth < 768;
+    var isTablet = window.innerWidth > 769 && window.innerWidth < 1025;
+    var isDesktop = window.innerWidth > 1024;
+    var adstest = getCookie('adstest');
+    var nc = isMobile && '7646-weatherMW' || '7646-weatherBW';
+    var src = "//ox-d.weatherus.servedbyopenx.com/w/1.0/jstag?nc=" + nc;
+    var openx = document.createElement('script');
+    var node = document.getElementsByTagName('script') && document.getElementsByTagName('script')[0];
+    var network, adUnit, adZone, NCTAU, adMapping, cust_params;
+    openx.src = src;
+    openx.async = true;
+    node.parentNode.insertBefore(openx, node);
+    $$.amznSlotsPromise = new TWC.Deferred();
+    $$.wxftgPromise = new TWC.Deferred();
+    $$.criteoPromise = new TWC.Deferred();
+
+    /** network */
+    network = "/7646/";
+    /** ad unit */
+    adUnit = isMobile ? "mobile_smart_us" : "web_weather_us";
+    adUnit = adstest ? 'test_' + adUnit : adUnit;
+    /** ad zone */
+    adZone = '/home';
+    NCTAU = network + adUnit + adZone;
+
+    adMapping = {
+        "desktop": {
+            "WX_WindowShade": {
+                pos: "wx_ws",
+                sizes: [[300,276],[728,91],[970,90],[728,50]]
+            },
+            "WX_Top300Variable": {
+                pos: "wx_300var",
+                sizes: [[300, 251],[300,600],[300,1050],[300,276]]
+            },
+            "WX_Mid300": {
+                pos: "wx_mid300",
+                sizes: [300, 251]
+            },
+            "WX_Hidden": {
+                pos: "wx_hdn",
+                sizes: [1,1]
+            }
+        },
+        "mobile": {
+            "MW_Position1": {
+                pos: "mw_p1",
+                sizes: [[320,51],[300,276],[300,45]]
+            },
+            "MW_Position2": {
+                pos: "mw_p2",
+                sizes: [300, 251]
+            }
+        }
+
+    };
+
+    cust_params =  {
+        cat: "fcst",
+        ch: "fcst",
+        fam: "fcst",
+        ad_unit: "%2F7646%2Fweb_weather_us"
+
+    };
+
+
+    if (adstest) {
+        cust_params['adstest'] = adstest;
+    }
+
+    var gptSlots = [];
+
+    function extend(){
+        for(var i=1; i<arguments.length; i++)
+            for(var key in arguments[i])
+                if(arguments[i].hasOwnProperty(key))
+                    arguments[0][key] = arguments[i][key];
+        return arguments[0];
+    }
+
+    function getCookie(name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
+    }
+
+    function setAd(el) {
+        var id = el.getAttribute('id');
+        var screen = isMobile && 'mobile' || 'desktop';
+        googletag.cmd.push(function () {
+            var slot = googletag.defineSlot(NCTAU, adMapping[screen][id].sizes, id)
+                .addService(googletag.pubads())
+                .setTargeting("pos", adMapping[screen][id].pos);
+            window.ls_dfp_slots.push(slot);
+            gptSlots.push(slot);
+        });
+    }
+
+    function displayAd(el) {
+        var id = el.getAttribute('id');
+        googletag.display(id);
+    }
+
+    $$.addLoadEvent(function() {
+        var selector = isMobile ? '[id^="MW_"]' : '[id^="WX_"]';
+        var adDivs = document.querySelectorAll(selector);
+        adDivs.forEach(function (el) {
+            setAd(el);
+        });
+
+        googletag.cmd.push(function () {
+            Promise.all([$$.wxftgPromise, $$.amznSlotsPromise]).then(function() {
+                $$.custParams = extend({}, $$.custParams, cust_params);
+                for (var p in $$.custParams) {
+                    if ($$.custParams.hasOwnProperty(p)) {
+                        googletag.pubads().setTargeting(p, $$.custParams[p]);
+                    }
+                }
+                googletag.pubads().enableSingleRequest();
+                googletag.enableServices();
+
+                if (typeof index_headertag_lightspeed !== 'undefined') {
+                    var cb = (function (adDivs, ls_dfp_slots) {
+                        return function () {
+                            index_headertag_lightspeed.set_slot_targeting(ls_dfp_slots)
+                            for (f = 0; f < adDivs.length; f++) {
+                                if (document.getElementById(adDivs[f].getAttribute('id'))) {
+                                    googletag.display(adDivs[f].getAttribute('id'));
+                                    document.getElementById("windowshade_info").innerHTML = "Pubads Call Time : " +  (new Date().getTime() - window.renderStartTime)/1000 + " secs";
+                                    console.dir("Pubads Call Time : " +  (new Date().getTime() - window.renderStartTime)/1000 + "secs");
+                                }
+                            }
+                        };
+                    })(adDivs, window.ls_dfp_slots);
+                    index_headertag_lightspeed.add_session_end_hook(cb, true);
+                    index_headertag_lightspeed.refresh();
+
+    //                        adDivs.forEach(function (el) {
+    //                            displayAd(el);
+    //                        });
+                } else {
+                    adDivs.forEach(function (el) {
+                        displayAd(el);
+                    });
+                }
+            });
+        //  });
+    });
+
+    });
+
+}(TWC.adUtils || {});
+
+
 // Weather FX Triggers Closure
 
 (function ($$) {
