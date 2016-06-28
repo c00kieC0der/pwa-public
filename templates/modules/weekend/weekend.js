0e52f35532a25072cf9ef497be140b59af668041
Will Bowling <will.bowling@weather.com>
2016-06-28 16:21:57 -0400

    more weekend stuffs

---
+++ templates/modules/weekend/weekend.js
@@ -0,0 +1,31 @@
+/**
+ * Created by ecook on 6/6/16.
+ */
+(function(){
+    var ngRepeatMap = [
+        ['js-day',        'dayPartName'],
+        ['js-date',        'validDate'],
+        ['js-wxicon',      'icon'],
+        ['js-temp',        'temperature'],
+        ['js-description', 'phrase'],
+        ['js-precip',      'precipPct'],
+        ['js-humidity',    'humidityPct']
+    ];
+    /*
+        What div,
+        what's the template's name?,
+        match data to div in the component,
+        data
+        do it how many times?
+     */
+    if(_Data.dailyForecast.day){
+        helper.ngRepeat('ls-row-wrap-24', 'ls-24-hour-data', ngRepeatMap, _Data.dailyForecast.day, 5);
+    } else {
+        helper.ngRepeat('ls-row-wrap-24', 'ls-24-hour-data', ngRepeatMap, _Data.dailyForecast.night, 5);
+    }
+
+    document.getElementById('event-anchor').addEventListener('builder', function() {
+        console.log('STUFF...', '_User.activeLocation.prsntNm');
+        helper.ngRepeat('ls-row-wrap-24', 'ls-24-hour-data', ngRepeatMap, _Data.dailyForecast.day, 5);
+    });
+})();
