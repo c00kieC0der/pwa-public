2ff6ee8137fe0949e1b3897ae7182bec464b2ce1
Omkar D <omkar.dokur.contractor@weather.com>
2016-07-03 23:30:12 -0400

    Fixed nav bug for desktop

--- js-src/appShell.js
+++ js-src/appShell.js
@@ -7,15 +7,19 @@ var downArrayClicked = false;http://localhost:5001/en-US/weather/today/l/
     /**
      * function to control the nav width
      */
-    window.onload = function () {
-    var navListEle = document.getElementsByClassName('page-nav-li');
-    var navUlEle = document.getElementsByClassName('page-nav');
-    var navItemwidth = 0;
-    for(i=0; i<navListEle.length; i++){
-        navItemwidth += navListEle[i].offsetWidth;
+    window.onload = navAppShell();
+    window.resize = navAppShell();
+
+
+    function navAppShell() {
+            var navListEle = document.getElementsByClassName('page-nav-li');
+            var navUlEle = document.getElementsByClassName('page-nav');
+            var navItemwidth = 0;
+            for(i=0; i<navListEle.length; i++){
+                navItemwidth += navListEle[i].offsetWidth;
+            }
+            navUlEle[0].style.width = navItemwidth + 'px';
     }
-    navUlEle[0].style.width = navItemwidth + 'px';
-};
 /**
  * showMainMenu() shows the hamburger menu when clicked
  */
