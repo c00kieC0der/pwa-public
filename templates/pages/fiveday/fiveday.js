0e8e0497bf1ad15d9619a6b1a774b1d0d40e9278
Erin Cook <erincook@ymaill.com>
2016-07-05 09:45:59 -0400

    add translations.

--- templates/pages/fiveday/fiveday.js
+++ templates/pages/fiveday/fiveday.js
@@ -3,13 +3,5 @@
  */
 (function(){
     helper.loadTemplate('5day-forecast', 'modules', '5day-forecast');
-
-    if(window['_Lang'] && window['_User'].activeLocation.prsntNm.length){ console.log('first run', _User.activeLocation.prsntNm);
-       // helper.setContent([['fiveday-title', _Lang['5 day forecast for {0}'].replace('{0}', _User.activeLocation.prsntNm)]]);
-    }
-    document.getElementById('event-anchor').addEventListener('builder', function(){ console.log('waited for event');
-        //helper.setContent([['fiveday-title', _Lang['5 day forecast for {0}'].replace('{0}', _User.activeLocation.prsntNm)]]);
-    });
-
     helper.loadTemplate('sun-moon', 'modules', 'sun-moon');
 })();
