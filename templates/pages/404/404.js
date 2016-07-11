b3d47584900feb3b810a32260d72066aa754a40a
Crystal Gardner <crystal.gardner@weather.com>
2016-07-11 15:48:28 -0400

    404 page setup

--- templates/pages/404/404.js
+++ templates/pages/404/404.js
@@ -2,5 +2,5 @@
  * Created by ecook on 6/19/16.
  */
 (function(){
-    helper.loadTemplate('error-module', 'modules', '404');
+    helper.loadTemplate('404-module', 'modules', '404-module');
 })();
