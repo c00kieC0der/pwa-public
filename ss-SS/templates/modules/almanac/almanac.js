1be9262bfb6db67a7740a42e6e96cec3e3e81b61
nxphu <xuanphu061@gmail.com>
2016-07-15 16:52:17 +0700

    WEB-7937: fix bug: Graphic issue under Almanac on Today page

--- ss-SS/templates/modules/almanac/almanac.js
+++ ss-SS/templates/modules/almanac/almanac.js
@@ -4,48 +4,88 @@
 (function(){
     var dataAssignmentAlmanac = [];
     var mapDataAlmanac = function(){
+        var DEG_SYMBOL = '&deg;';
+
+        var prevDayHigh     = _Data.reportedConditions.prevDayHigh,
+            prevDayLow      = _Data.reportedConditions.prevDayLow,
+            prevDayPrecip   = _Data.reportedConditions.prevDayPrecip,
+
+            sevenDayHigh    = _Data.reportedConditions.sevenDayHigh,
+            sevenDayLow     = _Data.reportedConditions.sevenDayLow,
+            sevenDayPrecip  = _Data.reportedConditions.sevenDayPrecip,
+
+            mtdHigh         = _Data.reportedConditions.mtdHigh,
+            mtdLow          = _Data.reportedConditions.mtdLow;
+            mtdPrecip       = _Data.reportedConditions.mtdPrecip;
+
+        var recordHigh      = _Data.oneDayHistorical.recordHigh,
+            recordHighYear  = _Data.oneDayHistorical.recordHighYear,
+            recordLow       = _Data.oneDayHistorical.recordLow,
+            recordLowYear   = _Data.oneDayHistorical.recordLowYear,
+
+            avgHigh         = _Data.oneDayHistorical.avgHigh,
+            avgLow          = _Data.oneDayHistorical.avgLow,
+            avgPrecip       = _Data.oneDayHistorical.avgPrecip;
+
+        var currentMonthAvgHigh     = _Data.historicalMonthlyAvg.currentMonthAvgHigh,
+            currentMonthAvgLow      = _Data.historicalMonthlyAvg.currentMonthAvgLow,
+            currentMonthAvgPrecip   = _Data.historicalMonthlyAvg.currentMonthAvgPrecip,
+
+            nextMonthAvgHigh        = _Data.historicalMonthlyAvg.nextMonthAvgHigh,
+            nextMonthAvgLow         = _Data.historicalMonthlyAvg.nextMonthAvgLow,
+            nextMonthAvgPrecip      = _Data.historicalMonthlyAvg.nextMonthAvgPrecip,
+
+            monthAfterNextAvgHigh   = _Data.historicalMonthlyAvg.monthAfterNextAvgHigh,
+            monthAfterNextAvgLow    = _Data.historicalMonthlyAvg.monthAfterNextAvgLow,
+            monthAfterNextAvgPrecip = _Data.historicalMonthlyAvg.monthAfterNextAvgPrecip;
+
         dataAssignmentAlmanac = [
-            ['almanac-prevday-avghigh',     _Data.reportedConditions.prevDayHigh + '&deg;'],
-            ['almanac-prevday-avglow',      _Data.reportedConditions.prevDayLow + '&deg;'],
-            ['almanac-prevday-avgprecip',   _Data.reportedConditions.prevDayPrecip + ' in'],
+            ['almanac-prevday-avghigh',     helper.safeDisplay(prevDayHigh) + (prevDayHigh ? DEG_SYMBOL : '')],
+            ['almanac-prevday-avglow',      helper.safeDisplay(prevDayLow) + (prevDayLow ? DEG_SYMBOL : '')],
+            ['almanac-prevday-avgprecip',   helper.safeDisplay(prevDayPrecip) + (prevDayPrecip ? DEG_SYMBOL : '')],
 
-            ['almanac-7day-avghigh',        _Data.reportedConditions.sevenDayHigh + '&deg;'],
-            ['almanac-7day-avglow',         _Data.reportedConditions.sevenDayLow + '&deg;'],
-            ['almanac-7day-avgprecip',      _Data.reportedConditions.sevenDayPrecip],
+            ['almanac-7day-avghigh',        helper.safeDisplay(sevenDayHigh) + (sevenDayHigh ? DEG_SYMBOL : '')],
+            ['almanac-7day-avglow',         helper.safeDisplay(sevenDayLow) + (sevenDayLow ? DEG_SYMBOL : '')],
+            ['almanac-7day-avgprecip',      helper.safeDisplay(sevenDayPrecip)],
 
-            ['almanac-mtd-high',            _Data.reportedConditions.mtdHigh + '&deg;'],
-            ['almanac-mtd-low',             _Data.reportedConditions.mtdLow + '&deg;'],
-            ['almanac-mtd-precip',          _Data.reportedConditions.mtdPrecip],
+            ['almanac-mtd-high',            helper.safeDisplay(mtdHigh) + (mtdHigh ? DEG_SYMBOL : '')],
+            ['almanac-mtd-low',             helper.safeDisplay(mtdLow) + (mtdLow ? DEG_SYMBOL : '')],
+            ['almanac-mtd-precip',          helper.safeDisplay(mtdPrecip)],
 
-            ['almanac-recordhigh',          _Data.oneDayHistorical.recordHigh + '&deg;'],
-            ['almanac-recordhighyr',        _Data.oneDayHistorical.recordHighYear],
-            ['almanac-recordlow',           _Data.oneDayHistorical.recordLow + '&deg;'],
-            ['almanac-recordlowyr',         _Data.oneDayHistorical.recordLowYear],
+            ['almanac-avghigh',             helper.safeDisplay(avgHigh) + (avgHigh ? DEG_SYMBOL : '')],
+            ['almanac-avglow',              helper.safeDisplay(avgLow) + (avgLow ? DEG_SYMBOL : '')],
+            ['almanac-avgprecip',           helper.safeDisplay(avgPrecip)],
 
-            ['almanac-avghigh',             _Data.oneDayHistorical.avgHigh + '&deg;'],
-            ['almanac-avglow',              _Data.oneDayHistorical.avgLow + '&deg;'],
-            ['almanac-avgprecip',           _Data.oneDayHistorical.avgPrecip],
+            ['almanac-month1-avghigh',      helper.safeDisplay(currentMonthAvgHigh) + (currentMonthAvgHigh ? DEG_SYMBOL : '')],
+            ['almanac-month1-avglow',       helper.safeDisplay(currentMonthAvgLow) + (currentMonthAvgLow ? DEG_SYMBOL : '')],
+            ['almanac-month1-avgprecip',    helper.safeDisplay(currentMonthAvgPrecip)],
 
-            ['almanac-month1-avghigh',      _Data.historicalMonthlyAvg.currentMonthAvgHigh + '&deg;'],
-            ['almanac-month1-avglow',       _Data.historicalMonthlyAvg.currentMonthAvgLow + '&deg;'],
-            ['almanac-month1-avgprecip',    _Data.historicalMonthlyAvg.currentMonthAvgPrecip],
+            ['almanac-month2-avghigh',      helper.safeDisplay(nextMonthAvgHigh) + (nextMonthAvgHigh ? DEG_SYMBOL : '')],
+            ['almanac-month2-avglow',       helper.safeDisplay(nextMonthAvgLow) + (nextMonthAvgLow ? DEG_SYMBOL : '')],
+            ['almanac-month2-avgprecip',    helper.safeDisplay(nextMonthAvgPrecip)],
 
-            ['almanac-month2-avghigh',      _Data.historicalMonthlyAvg.nextMonthAvgHigh + '&deg;'],
-            ['almanac-month2-avglow',       _Data.historicalMonthlyAvg.nextMonthAvgLow + '&deg;'],
-            ['almanac-month2-avgprecip',    _Data.historicalMonthlyAvg.nextMonthAvgPrecip],
-
-            ['almanac-month3-avghigh',      _Data.historicalMonthlyAvg.monthAfterNextAvgHigh + '&deg;'],
-            ['almanac-month3-avglow',       _Data.historicalMonthlyAvg.monthAfterNextAvgLow + '&deg;'],
-            ['almanac-month3-avgprecip',    _Data.historicalMonthlyAvg.monthAfterNextAvgPrecip],
+            ['almanac-month3-avghigh',      helper.safeDisplay(monthAfterNextAvgHigh) + (monthAfterNextAvgHigh ? DEG_SYMBOL : '')],
+            ['almanac-month3-avglow',       helper.safeDisplay(monthAfterNextAvgLow) + (monthAfterNextAvgLow ? DEG_SYMBOL : '')],
+            ['almanac-month3-avgprecip',    helper.safeDisplay(monthAfterNextAvgPrecip)],
 
             //TODO: Translate these
-            ['almanac-currentmonth',        _Data.almanacMonths.month1],
-            ['almanac-nextmonth',           _Data.almanacMonths.month2],
-            ['almanac-monthafternext',      _Data.almanacMonths.month3],
+            ['almanac-currentmonth',        helper.safeDisplay(_Data.almanacMonths.month1)],
+            ['almanac-nextmonth',           helper.safeDisplay(_Data.almanacMonths.month2)],
+            ['almanac-monthafternext',      helper.safeDisplay(_Data.almanacMonths.month3)],
 
-            ['almanac-date',                _Data.almanacMonths.currentDate]
+            ['almanac-date',                helper.safeDisplay(_Data.almanacMonths.currentDate)]
 
         ];
+
+        var recordsArray = [
+            ['almanac-recordhigh',          helper.safeDisplay(recordHigh) + (recordHigh ? DEG_SYMBOL : '')],
+            ['almanac-recordlow',           helper.safeDisplay(recordLow) + (recordLow ? DEG_SYMBOL : '')]
+        ];
+        if (recordHighYear || recordLowYear) {
+            recordsArray.push(['almanac-recordhighyr', helper.safeDisplay(recordHighYear)]);
+            recordsArray.push(['almanac-recordlowyr',  helper.safeDisplay(recordLowYear)]);
+        }
+        dataAssignmentAlmanac = dataAssignmentAlmanac.concat(recordsArray);
         helper.setContent(dataAssignmentAlmanac);
     };
 
